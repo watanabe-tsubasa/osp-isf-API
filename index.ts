390ae5fdaa6d0127d0b8d2a944ccdff51246b622
Tsubasa Watanabe <t.watanabe423@gmail.com>
2023-10-23 09:45:58 +0000

    upd

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
-const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
+const baseUrl = 'https://api.sandbox.atmosphere.osp.tech';
+
+const res = await fetch(`${baseUrl}/v3/products`, {
+  method: 'GET',
+  // body: JSON.stringify({
+  //   title: 'foo',
+  //   body: 'bar',
+  //   userId: 1,
+  // }),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
+    'x-api-key': 'js;afjalfjsa;ofjoasja',
+    'Authorization': 'Bearer sfjsl;afjasof:sjf:asj:f',
+    'RequestId': '48379279470921'
   },
 })
 const json = await res.json();
-console.log(json);
+const header = res.headers;
+const status = res.status;
+console.log(json, header, status);
