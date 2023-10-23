a01380f39269b5356bc9854c4fbf569789338285
Tsubasa Watanabe <t.watanabe423@gmail.com>
2023-10-23 09:33:31 +0000

    upd

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+const json = await res.json();
+console.log(json);
