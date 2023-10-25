29bd13bbb7b75f1c2e43249be77d236dda8ef752
Tsubasa Watanabe <t.watanabe423@gmail.com>
2023-10-25 08:02:16 +0000

    upd

--- index.ts
+++ index.ts
@@ -1,20 +1,33 @@
 const baseUrl = 'https://api.sandbox.atmosphere.osp.tech';
 
+// 環境変数の存在を確認
+const apiKey = process.env.API_KEY;
+if (!apiKey) {
+    throw new Error("API_KEY is not set");
+}
+
+const accessToken = process.env.ACCESS_TOKEN;
+if (!accessToken) {
+    throw new Error("ACCESS_TOKEN is not set");
+}
+
+const requestId = process.env.REQUEST_ID;
+if (!requestId) {
+    throw new Error("REQUEST_ID is not set");
+}
+
 const res = await fetch(`${baseUrl}/v3/products`, {
   method: 'GET',
-  // body: JSON.stringify({
-  //   title: 'foo',
-  //   body: 'bar',
-  //   userId: 1,
-  // }),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
-    'x-api-key': 'js;afjalfjsa;ofjoasja',
-    'Authorization': 'Bearer sfjsl;afjasof:sjf:asj:f',
-    'RequestId': '48379279470921'
+    'x-api-key': apiKey,
+    'Authorization': `Bearer ${accessToken}`,
+    'RequestId': requestId
   },
-})
+});
+
 const json = await res.json();
 const header = res.headers;
 const status = res.status;
+
 console.log(json, header, status);
